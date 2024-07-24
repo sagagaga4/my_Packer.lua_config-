253535ac8c78c0fab0d77fd27c12180fc45ad15d
‪Sagi Nachum‬‏ <57183982+sagagaga4@users.noreply.github.com>
2024-07-24 19:35:47 +0300

    Create my_config_packer.lua

---
+++ my_config_packer.lua
@@ -0,0 +1,96 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+
+	use {
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.8',
+		-- or
+		-- branch = '0.1.x',
+		requires = { 'nvim-lua/plenary.nvim' }
+	}
+	    -- Color schemes
+    use 'erikbackman/brightburn.vim'
+
+    use {
+        'folke/tokyonight.nvim',
+        config = function()
+            require("tokyonight").setup({
+                style = "storm",
+                transparent = true,
+                terminal_colors = true,
+                styles = {
+                    comments = { italic = false },
+                    keywords = { italic = false },
+                    sidebars = "dark",
+                    floats = "dark",
+                },
+            })
+            vim.cmd('colorscheme tokyonight')
+        end
+    }
+
+    use {
+        'ellisonleao/gruvbox.nvim',
+        as = 'gruvbox',
+        config = function()
+            require("gruvbox").setup({
+                terminal_colors = true,
+                undercurl = true,
+                underline = false,
+                bold = true,
+                italic = {
+                    strings = false,
+                    emphasis = false,
+                    comments = false,
+                    operators = false,
+                    folds = false,
+                },
+                strikethrough = true,
+                invert_selection = false,
+                invert_signs = false,
+                invert_tabline = false,
+                invert_intend_guides = false,
+                inverse = true,
+                contrast = "",
+                palette_overrides = {},
+                overrides = {},
+                dim_inactive = false,
+                transparent_mode = false,
+            })
+     end
+     }
+
+	use({
+		'rose-pine/neovim',
+		as = 'rose-pine',
+		config = function()
+			vim.cmd('colorscheme rose-pine')
+		end
+	})
+	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+	use('nvim-treesitter/playground')
+	use('theprimeagen/harpoon')
+	use('mbbill/undotree')
+	use('tpope/vim-fugitive')
+	use {
+'VonHeikemen/lsp-zero.nvim',
+		branch = 'v3.x',
+		requires = {
+			{'williamboman/mason.nvim'},
+			{'williamboman/mason-lspconfig.nvim'},
+
+			{'neovim/nvim-lspconfig'},
+			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'L3MON4D3/LuaSnip'},
+		}
+	}
+
+	-- Add more plugins as needed
+end) -- Remove this line
